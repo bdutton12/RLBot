af65e4c9a4856512bba2f5add260bb082a0395d5
duttonb2 <duttonb2@msu.edu>
2023-10-20 11:04:12 -0400

    feat: Better commenting

--- model_test.py
+++ model_test.py
@@ -11,15 +11,22 @@ from rlgym.utils.gamestates import GameState
 
 from model.agent import Agent
 
-
+"""
+Defines a class for RLGym that determines when to end an episode
+"""
 class CustomTerminalCondition(TerminalCondition):
     def reset(self, initial_state: GameState):
         pass
 
     def is_terminal(self, current_state: GameState) -> bool:
-        return current_state.last_touch != -1
+        return current_state.last_touch != -1 # End episode when the ball is touched
 
+"""
+Observation Object for RLGym
 
+Gets current state of ball and all players and inverts the values if bot is on team Orange
+so that the model doesn't need to worry about team
+"""
 class CustomObsBuilderBluePerspective(ObsBuilder):
     def reset(self, initial_state: GameState):
         pass
@@ -47,11 +54,13 @@ class CustomObsBuilderBluePerspective(ObsBuilder):
 
 
 if __name__ == "__main__":
+    # Instantiate RLGym environment
     env = rlgym.make(
         obs_builder=CustomObsBuilderBluePerspective(),
         terminal_conditions=[CustomTerminalCondition()],
         reward_fn=VelocityReward(),
     )
+
     N = 20
     batch_size = 5
     n_epochs = 4
@@ -73,22 +82,39 @@ if __name__ == "__main__":
     n_steps = 0
 
     for i in range(n_games):
+        # Reset environment to default values for each game (episode)
         observation = env.reset()
+
         done = False
         score = 0
         while not done:
+
+            # Agent returns a vector [throttle, steer, yaw, pitch, roll, jump, boost, handbrake]
+            # The first five are in the range [-1, 1], the latter are boolean
             action, prob, val = agent.choose_action(observation)
+
+            # Updated game state, reward, and whether the episode is done is retrieved
             observation_, reward, done, info = env.step(action)
+            
             n_steps += 1
+            
+            # The reward for all actions in an episode are summed
             score += reward
-            agent.store_to_memory(observation, action, prob, val, reward, done)
+
+            # The game state, action taken, probability of success, and reward are stored to memory
+            agent.store_to_memory(observation, action, prob, val, reward, done)0
+            
+            # Every N steps, a learning iteration is done
             if n_steps % N == 0:
                 agent.learn()
                 learn_iters += 1
             observation = observation_
+        
+        # Score history is updated and the average of the last 100 scores is taken
         score_history.append(score)
         avg_score = np.mean(score_history[-100:])
 
+        # The model is saved to a file if it is a better solution
         if avg_score > best_score:
             best_score = avg_score
             agent.save_models()

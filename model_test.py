02691a9e88616c95fd99567ed86e8a624c9b484d
duttonb2 <duttonb2@msu.edu>
2023-10-20 10:48:37 -0400

    feat: Switch gym to rlgym and simple reward/terminal/observation objects

--- model_test.py
+++ model_test.py
@@ -1,46 +1,105 @@
 import gym
 import numpy as np
 
+import rlgym
+from rlgym.utils.reward_functions.common_rewards import VelocityReward
+from rlgym.utils.obs_builders import ObsBuilder
+from rlgym.utils import common_values
+from rlgym.utils.gamestates import PlayerData, GameState
+from rlgym.utils.terminal_conditions import TerminalCondition
+from rlgym.utils.gamestates import GameState
+
 from model.agent import Agent
 
-if __name__ == '__main__':
-  env = gym.make('CartPole-v0')
-  N = 20
-  batch_size = 5
-  n_epochs = 4
-  eta = 0.0003
-  agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, 
-                  eta=eta, n_epochs=n_epochs, 
-                  input_dims=env.observation_space.shape)
-  n_games = 300
 
-  best_score = env.reward_range[0]
-  score_history = []
+class CustomTerminalCondition(TerminalCondition):
+    def reset(self, initial_state: GameState):
+        pass
 
-  learn_iters = 0
-  avg_score = 0
-  n_steps = 0
+    def is_terminal(self, current_state: GameState) -> bool:
+        return current_state.last_touch != -1
 
-  for i in range(n_games):
-      observation = env.reset()
-      done = False
-      score = 0
-      while not done:
-          action, prob, val = agent.choose_action(observation)
-          observation_, reward, done, info = env.step(action)
-          n_steps += 1
-          score += reward
-          agent.store_to_memory(observation, action, prob, val, reward, done)
-          if n_steps % N == 0:
-              agent.learn()
-              learn_iters += 1
-          observation = observation_
-      score_history.append(score)
-      avg_score = np.mean(score_history[-100:])
 
-      if avg_score > best_score:
-          best_score = avg_score
-          agent.save_models()
+class CustomObsBuilderBluePerspective(ObsBuilder):
+    def reset(self, initial_state: GameState):
+        pass
 
-      print('episode', i, 'score %.1f' % score, 'avg score %.1f' % avg_score,
-              'time_steps', n_steps, 'learning_steps', learn_iters)
+    def build_obs(
+        self, player: PlayerData, state: GameState, previous_action: np.ndarray
+    ) -> Any:
+        obs = []
+
+        # If this observation is being built for a player on the orange team, we need to invert all the physics data we use.
+        inverted = player.team_num == common_values.ORANGE_TEAM
+
+        if inverted:
+            obs += state.inverted_ball.serialize()
+        else:
+            obs += state.ball.serialize()
+
+        for player in state.players:
+            if inverted:
+                obs += player.inverted_car_data.serialize()
+            else:
+                obs += player.car_data.serialize()
+
+        return np.asarray(obs, dtype=np.float32)
+
+
+if __name__ == "__main__":
+    env = rlgym.make(
+        obs_builder=CustomObsBuilderBluePerspective(),
+        terminal_conditions=[CustomTerminalCondition()],
+        reward_fn=VelocityReward(),
+    )
+    N = 20
+    batch_size = 5
+    n_epochs = 4
+    eta = 0.0003
+    agent = Agent(
+        n_actions=env.action_space.n,
+        batch_size=batch_size,
+        eta=eta,
+        n_epochs=n_epochs,
+        input_dims=env.observation_space.shape,
+    )
+    n_games = 300
+
+    best_score = env.reward_range[0]
+    score_history = []
+
+    learn_iters = 0
+    avg_score = 0
+    n_steps = 0
+
+    for i in range(n_games):
+        observation = env.reset()
+        done = False
+        score = 0
+        while not done:
+            action, prob, val = agent.choose_action(observation)
+            observation_, reward, done, info = env.step(action)
+            n_steps += 1
+            score += reward
+            agent.store_to_memory(observation, action, prob, val, reward, done)
+            if n_steps % N == 0:
+                agent.learn()
+                learn_iters += 1
+            observation = observation_
+        score_history.append(score)
+        avg_score = np.mean(score_history[-100:])
+
+        if avg_score > best_score:
+            best_score = avg_score
+            agent.save_models()
+
+        print(
+            "episode",
+            i,
+            "score %.1f" % score,
+            "avg score %.1f" % avg_score,
+            "time_steps",
+            n_steps,
+            "learning_steps",
+            learn_iters,
+        )

f2247d34e9b25e44a9124bca32c095e347effd91
Brandon Dutton <bdutton12@outlook.com>
2023-11-08 11:24:00 -0500

    Switch to SB3 with multi-instance self play training

--- bot.py
+++ bot.py
@@ -1,33 +1,90 @@
+import gym
+import numpy as np
+
 import rlgym
-from model.agent import Agent
+from rlgym.utils.reward_functions.common_rewards import VelocityReward
+from rlgym.utils.obs_builders import ObsBuilder
+from rlgym.utils.state_setters import DefaultState
+from rlgym.utils import common_values
+from rlgym.utils.gamestates import PlayerData, GameState
+from rlgym.utils.terminal_conditions import TerminalCondition
+from rlgym.utils.gamestates import GameState
+
+from rlgym.envs import Match
+from rlgym_tools.sb3_utils import SB3MultipleInstanceEnv
+
+from stable_baselines3 import PPO
+
 
 """
-Main driver of the program 
+Defines a class for RLGym that determines when to end an episode
 """
-def main():
 
 
-    # Create env and model
-    env = rlgym.make()
-    N = 20
-    batch_size = 5
-    n_epochs = 4
-    eta = 0.0003
-    agent = Agent(n_actions=env.action_space.n, batch_size=batch_size, 
-                eta=eta, n_epochs=n_epochs, 
-                input_dims=env.observation_space.shape)
+class TerminalConditions(TerminalCondition):
+    def reset(self, initial_state: GameState):
+        pass
 
-    while True:
-        obs = env.reset()
-        done = False
-        print("Hello World")
+    def is_terminal(self, current_state: GameState) -> bool:
+        return current_state.last_touch != -1  # End episode when the ball is touched
 
-        while not done:
-            # Here we sample a random action. If you have an agent, you would get an action from it here.
-            action = env.action_space.sample()
 
-            next_obs, reward, done, gameinfo = env.step(action)
+"""
+Observation Object for RLGym
 
-            obs = next_obs
+Gets current state of ball and all players and inverts the values if bot is on team Orange
+so that the model doesn't need to worry about team
+"""
 
-main()
+
+class UnbiasedObservationBuilder(ObsBuilder):
+    def reset(self, initial_state: GameState):
+        pass
+
+    def build_obs(
+        self, player: PlayerData, state: GameState, previous_action: np.ndarray
+    ) -> Any:
+        obs = []
+
+        # If this observation is being built for a player on the orange team, we need to invert all the physics data we use.
+        inverted = player.team_num == common_values.ORANGE_TEAM
+
+        if inverted:
+            obs += state.inverted_ball.serialize()
+        else:
+            obs += state.ball.serialize()
+
+        for player in state.players:
+            if inverted:
+                obs += player.inverted_car_data.serialize()
+            else:
+                obs += player.car_data.serialize()
+
+        return np.asarray(obs, dtype=np.float32)
+
+
+"""
+Initialize an instance of Rocket League
+"""
+
+
+def get_match():
+    return Match(
+        reward_function=VelocityReward(),
+        terminal_conditions=[TerminalConditions()],
+        obs_builder=UnbiasedObservationBuilder(),
+        state_setter=DefaultState(),
+        self_play=True,
+    )
+
+
+if __name__ == "__main__":
+    # Make the default rlgym environment
+
+    # Initialize PPO from stable_baselines3
+    env = SB3MultipleInstanceEnv(
+        match_func_or_matches=get_match, num_instances=2, wait_time=20
+    )
+
+    learner = PPO(policy="MlpPolicy", env=env, verbose=1)
+    learner.learn(1_000_000)

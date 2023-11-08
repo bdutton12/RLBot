4a6959ca8be71e8ab8eb3d1e140282b22627521a
duttonb2 <duttonb2@msu.edu>
2023-11-08 18:12:48 -0500

    fix: SB3 Version errors

--- bot.py
+++ bot.py
@@ -1,6 +1,4 @@
-import gym
 import numpy as np
-
 import rlgym
 from rlgym.utils.reward_functions.common_rewards import VelocityReward
 from rlgym.utils.obs_builders import ObsBuilder
@@ -10,12 +8,13 @@ from rlgym.utils.gamestates import PlayerData, GameState
 from rlgym.utils.terminal_conditions import TerminalCondition
 from rlgym.utils.gamestates import GameState
 from rlgym.utils.terminal_conditions.common_conditions import TimeoutCondition
+from rlgym.utils.action_parsers import DefaultAction
+from reward_functions import MoveTowardsGoal
 
 from rlgym.envs import Match
-from rlgym_tools.sb3_utils import SB3MultipleInstanceEnv
+from rlgym_tools.sb3_utils import SB3SingleInstanceEnv, SB3MultipleInstanceEnv
 
-from stable_baselines3 import PPO
-
+from stable_baselines3.ppo import PPO
 
 """
 Defines a class for RLGym that determines when to end an episode
@@ -45,7 +44,7 @@ class UnbiasedObservationBuilder(ObsBuilder):
 
     def build_obs(
         self, player: PlayerData, state: GameState, previous_action: np.ndarray
-    ) -> Any:
+    ):
         obs = []
 
         # If this observation is being built for a player on the orange team, we need to invert all the physics data we use.
@@ -76,19 +75,30 @@ def get_match():
         terminal_conditions=[TerminalConditions(), TimeoutCondition(500)],
         obs_builder=UnbiasedObservationBuilder(),
         state_setter=DefaultState(),
-        self_play=True,
+        action_parser=DefaultAction(),
+        spawn_opponents=True,
     )
 
 
 if __name__ == "__main__":
-    # Make the default rlgym environment
-
-    # Initialize PPO from stable_baselines3
-    env = SB3MultipleInstanceEnv(
-        match_func_or_matches=get_match, num_instances=2, wait_time=20
+    # Make RLGym environment
+    gym_env = rlgym.make(
+        reward_fn=MoveTowardsGoal(),
+        obs_builder=UnbiasedObservationBuilder(),
+        terminal_conditions=TerminalConditions(),
+        use_injector=True,
+        spawn_opponents=True,
     )
 
+    # Change to SB3 instance wrapper to allow self-play
+    env = SB3SingleInstanceEnv(gym_env)
+
+    # If a saved model exists, load that and overwrite empty model
     learner = PPO(policy="MlpPolicy", env=env, verbose=1)
     learner.load("./saved_model/PPO_model.zip", env=env)
+
+    # Learn
     learner.learn(1_000_000)
+
+    # Save model
     learner.save("./saved_model/PPO_model.zip")
